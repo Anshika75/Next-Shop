b7c38bc4a9d4068ee4d7d6b5af8966e647a43ba8
Anshika <anshikathakur596@gmail.com>
2023-04-25 14:44:12 +0530

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -24,7 +24,7 @@ function SignUp() {
         <input className="px-[3vmin] py-[2vmin] my-[2vmin] bg-[#f2f2f2] transition-all hover:outline-[#651a57] focus:outline-[#651a57]" type="email" placeholder="Email" value={email} onChange={(e)=>setemail(e.target.value)} required/>
         <input className="px-[3vmin] py-[2vmin] my-[2vmin] bg-[#f2f2f2] transition-all hover:outline-[#651a57] focus:outline-[#651a57]" type="password" placeholder="Password" value={password} onChange={(e)=>setpassword(e.target.value)} required/>
         <button className="primary text-white primaryfont px-[4vmin] py-[1vmin] rounded-full hover:bg-[#651a57bb] m-[2vmin] transition-all hover:scale-90 hover:opacity-90">SignUp</button>
-        <Link className="primaryfont" href={"/signup"}>Already Have A Account? <span className="text-[#651a57]">Login</span> </Link>
+        <Link className="primaryfont" href={"/login"}>Already have an account? <span className="text-[#651a57]">Login</span> </Link>
       </form>
     </div>
   );
