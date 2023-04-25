a9cb6863a088e8c15b5b592b1dcc2aff46ffe346
I-sandesh <83366556+I-Himanshu@users.noreply.github.com>
2023-04-25 16:30:39 +0530

    Add product is working now

--- firebase/config.js
+++ firebase/config.js
@@ -18,3 +18,4 @@ export default app;
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const categoryRef = collection(db,"categories");
+export const productRef = collection(db,"products");
---
+++ firebase/products.js
@@ -0,0 +1,32 @@
+import { productRef } from "./config";
+import { getDocs, query, where, collection, getDoc, doc, addDoc } from "firebase/firestore";
+
+export const getProducts = async () => {
+    const products = [];
+    const querySnapshot = await getDocs(productRef);
+    querySnapshot.forEach((doc) => {
+        products.push({ id: doc.id, ...doc.data() });
+    });
+    return products;
+}
+
+export const getProductsByCategory = async (category) => {
+    const products = [];
+    const q = query(productRef, where("category", "==", category));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        products.push({ id: doc.id, ...doc.data() });
+    });
+    return products;
+}
+
+export const createProduct = async(name,price,description,category,image)=>{
+    const docRef = await addDoc(productRef,{
+        name,
+        price,
+        description,
+        category,
+        image
+    });
+    return docRef.id;
+}
